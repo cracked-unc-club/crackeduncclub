de5b6c897084e1367836dcf883e59eed6a7986f7
lowercasenamehere <awp.lowercase@gmail.com>
2024-09-02 13:00:30 -0700

    rendering :check: :check:

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,10 @@
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+import cloudflare from '@astrojs/cloudflare';
+
+// setup on demand rendering https://docs.astro.build/en/guides/server-side-rendering/
+// cred: @meerkatDotTs
+export default defineConfig({
+    output: 'server',
+    adapter: cloudflare()
+});
