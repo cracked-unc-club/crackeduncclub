2938321cf892f93f3f0ec246e19c5a5b8a248c28
lowercasenamehere <awp.lowercase@gmail.com>
2024-09-02 21:04:59 -0700

    resources.json fetch and render client side fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,4 @@
 import { defineConfig } from 'astro/config';
 
-import cloudflare from '@astrojs/cloudflare';
-
-// setup on demand rendering https://docs.astro.build/en/guides/server-side-rendering/
-// cred: @meerkatDotTs
-export default defineConfig({
-    output: 'server',
-    adapter: cloudflare()
-});
+// https://astro.build/config
+export default defineConfig({});
